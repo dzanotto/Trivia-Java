a3e69442d102c41e57d2dc5d7dbb34d6a1b080dc
dzanotto <d.zanotto@gmail.com>
2024-11-29 17:22:32 +0100

    Structure for golden rule

--- src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,14 +1,28 @@
 package com.adaptionsoft.games.trivia;
 
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+import org.approvaltests.Approvals;
+import org.approvaltests.reporters.DiffReporter;
+import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
+@UseReporter(DiffReporter.class)
 public class SomeTest {
 
     @Test
-    public void true_is_true() throws Exception {
-        assertTrue(false);
+    public void run() throws Exception {
+        ByteArrayOutputStream fakeoutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeoutput));
+        System.setIn(new ByteArrayInputStream("a\n".getBytes()));
+
+        GameRunner.main(new String[]{});
+        String output = fakeoutput.toString();
+
+        Approvals.verify(output);
     }
 }
